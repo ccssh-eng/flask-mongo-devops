e06b327b671a3e2ea85f70056c8e5d456b126f56
ccssh-eng <cirdec.labhub@gmail.com>
2025-07-28 17:25:02 +0200

    Correction d'app.py

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ client = MongoClient(mongo_uri)
 db = client["devops"]
 collection = db["items"]
 
+
 @app.route("/", methods=["GET"])
 def home():
     return "Flask MongoDB DevOps API is running"
+
 
 @app.route("/add", methods=["POST"])
 def add_item():
@@ -22,10 +24,13 @@ def add_item():
     item_id = collection.insert_one({"name": data["name"]}).inserted_id
     return jsonify({"message": "Item added", "id": str(item_id)}), 201
 
+
 @app.route("/items", methods=["GET"])
 def get_items():
-    items = [{"id": str(doc["_id"]), "name": doc["name"]} for doc in collection.find()]
+    items = [{"id": str(doc["_id"]), "name": doc["name"]}
+             for doc in collection.find()]
     return jsonify(items)
+
 
 @app.route("/delete/<string:item_name>", methods=["DELETE"])
 def delete_item(item_name):
@@ -33,6 +38,7 @@ def delete_item(item_name):
     if result.deleted_count:
         return jsonify({"message": f"Deleted item: {item_name}"}), 200
     return jsonify({"error": "Item not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
